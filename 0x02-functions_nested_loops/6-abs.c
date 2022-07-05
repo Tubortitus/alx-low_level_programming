348df97710d499d1a6753301db69ca23927fb300
Titus Ojediran <olatubor@gmail.com>
2022-07-05 11:20:32 -0700

    advance task to go,so tiring

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: integer to be checked
+ *
+ * Created - Titus Ojediran
+ * cc: 5th july, 2022
+ *
+ * Return: the absolute value
+ */
+
+int _abs(int n)
+{
+
+	return ((n <= 0) ? n * -1 : n);
+}
