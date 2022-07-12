a88e3fbb06511aca6ff4752019739a4e197a8bfd
Titus Ojediran <olatubor@gmail.com>
2022-07-12 10:06:32 -0700

    coding

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,12 +4,10 @@
  * reset_to_98 - takes a pointer to a variable and changes it value
  * @n: value to be change
  *
- * Created by Titus Ojediran 
+ * Created by: Titus
  * cc: 12th july, 2022.
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
-
