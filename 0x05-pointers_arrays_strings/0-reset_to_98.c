6ce2711793ca2b45ca405eadba30733fa07809df
Titus Ojediran <olatubor@gmail.com>
2022-07-12 09:15:57 -0700

    starting to code today

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to a variable and changes it value
+ * @n: value to be change
+ *
+ * Created by Titus Ojediran 
+ * cc: 12th july, 2022.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - takes two integer variable and swap them
+ * @var1: first variable
+ * @var2: second variable
+ *
+ * Created by: Titus Ojediran 
+ * cc: 12th july, 2022
+ */
+
+void swap_int(int *var1, int *var2)
+{
+	int tmp = *var1;
+	*var1 = *var2;
+	*var2 = tmp;
+}
+
